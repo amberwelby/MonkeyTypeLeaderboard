6f6e7efe9a5fef7ea56de04ecb3f9306d912cb24
mkk100 <tommychimes2003@gmail.com>
2024-09-14 10:36:35 -0700

    added NextUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import LeaderBoard from "@/components/LeaderBoard";
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
-  return <div className="bg-red-900">hi</div>;
+  return (
+    <div className="bg-red-900">
+      <LeaderBoard />
+    </div>
+  );
 }
