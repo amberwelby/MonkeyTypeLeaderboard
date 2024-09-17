58c5cbb45e7ab3f2d64aa113390f312a550f51d0
mkk100 <tommychimes2003@gmail.com>
2024-09-16 21:09:11 -0700

    implemented sorting, adding players, retrieving players all in localstorage

--- src/components/LeaderBoard.tsx
+++ src/components/LeaderBoard.tsx
@@ -8,27 +8,68 @@ import {
   TableRow,
   TableCell,
   Button,
+  Input,
 } from "@nextui-org/react";
-
-const mockData = [
-  { id: 1, name: "Tony Reichert", wpm15: 85, wpm30: 90 },
-  { id: 2, name: "Zoey Lang", wpm15: 92, wpm30: 70 },
-  { id: 3, name: "Jane Fisher", wpm15: 78, wpm30: 80 },
-  { id: 4, name: "William Howard", wpm15: 88, wpm30: 50 },
-  { id: 5, name: "Emma Johnson", wpm15: 95, wpm30: 100 },
-];
+import axios from "axios";
+const mockData: string[] = [];
 
 const LeaderBoard = () => {
   const [sortedData, setSortedData] = useState(mockData);
-  const [selectedColor, setSelectedColor] = React.useState("default");
+  const [userName, setUserName] = useState("");
+  const retrieve = async () => {
+    const existingArray =
+      JSON.parse(localStorage.getItem("usernames") || "null") || [];
+    const data = [];
+    for (let i = 0; i < existingArray.length; i++) {
+      const res = await axios.get(
+        "https://api.monkeytype.com/users/" + existingArray[i] + "/profile"
+      );
+      data.push({
+        name: existingArray[i],
+        wpm15: res.data.data.personalBests.time["15"]["0"]["wpm"],
+        wpm30: res.data.data.personalBests.time["30"]["0"]["wpm"],
+      });
+    }
+    setSortedData(data);
+  };
+  const addEntry = async () => {
+    const res = await axios.get(
+      "https://api.monkeytype.com/users/" + userName + "/profile"
+    );
+
+    const existingArray =
+      JSON.parse(localStorage.getItem("usernames") || "null") || [];
+
+    existingArray.push(userName);
+
+    localStorage.setItem("usernames", JSON.stringify(existingArray));
+
+    const newEntry = {
+      name: userName,
+      wpm15: res.data.data.personalBests.time["15"]["0"]["wpm"],
+      wpm30: res.data.data.personalBests.time["30"]["0"]["wpm"],
+    };
+    setSortedData([...sortedData, newEntry]);
+  };
 
   const sortData = (key: string) => {
     const sorted = [...sortedData].sort((a, b) => b[key] - a[key]);
     setSortedData(sorted);
   };
-
+  const getUserName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(e.target.value);
+  };
   return (
     <div className="flex justify-center items-center min-h-screen">
+      <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+        <Input type="text" label="User Name" onChange={getUserName} />
+      </div>
+      <Button color="primary" onClick={addEntry}>
+        Add Player
+      </Button>
+      <Button color="primary" onClick={retrieve}>
+        Retrieve
+      </Button>
       <div>
         <Table
           aria-label="Example static collection table"
